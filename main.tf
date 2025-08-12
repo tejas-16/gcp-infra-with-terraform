6e715c498a1d09a4b5ff32a1d2f221860178c1b4
tejas-16 <74181918+tejas-16@users.noreply.github.com>
2025-08-12 23:16:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,6 @@ resource "google_compute_subnetwork" "custom_subnet" {
   name          = var.subnet_name
   ip_cidr_range = "10.0.0.0/24"
   region        = var.region
-  zone    = var.zone
   network       = google_compute_network.custom_vpc.id
 }
 
@@ -17,6 +16,7 @@ resource "google_compute_subnetwork" "custom_subnet" {
 resource "google_compute_instance" "vm_instance" {
   name         = var.instance_name
   machine_type = "e2-micro"
+  zone    = var.zone
 
   boot_disk {
     initialize_params {
