89fbf6ed647506bbaba5a1e3dfd664e10be4c76d
tejas-16 <74181918+tejas-16@users.noreply.github.com>
2025-08-12 20:58:10 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,30 @@
 variable "project_id" {
-  type = string
+  type        = string
+  description = "GCP Project ID"
+}
+
+variable "region" {
+  type        = string
+  default     = "us-central1"
 }
 
 variable "instance_name" {
-  type = string
+  type        = string
 }
 
 variable "vpc_name" {
-  type = string
+  type        = string
 }
 
 variable "subnet_name" {
-  type = string
+  type        = string
 }
 
 variable "bucket_name" {
-  type = string
+  type        = string
+}
+
+variable "bucket_name_state" {
+  type        = string
+  description = "Name of the GCS bucket for Terraform state"
 }
