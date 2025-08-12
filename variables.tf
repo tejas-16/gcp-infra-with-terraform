1294c5d0db9ee8b2f2fb9db50c1ae75eeab0cfd6
tejas-16 <74181918+tejas-16@users.noreply.github.com>
2025-08-12 19:49:43 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "The GCP zone"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "gcp_credentials_file" {
+  description = "Path to the GCP service account key file"
+  type        = string
+}
