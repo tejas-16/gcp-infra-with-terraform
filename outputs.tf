1996bcf4264d9adad1dec242fdcf6b2e8adf7312
tejas-16 <74181918+tejas-16@users.noreply.github.com>
2025-08-12 22:44:33 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,4 @@
-output "instance_name" {
-  description = "Name of the created VM instance"
-  value       = google_compute_instance.default.name
-}
-
 output "instance_ip" {
-  description = "Public IP of the created VM instance"
-  value       = google_compute_instance.default.network_interface[0].access_config[0].nat_ip
-}
-
-output "bucket_name" {
-  description = "Name of the created storage bucket"
-  value       = google_storage_bucket.default.name
-}
-
-output "vpc_name" {
-  description = "Name of the created VPC"
-  value       = google_compute_network.default.name
-}
-
-output "subnet_name" {
-  description = "Name of the created subnet"
-  value       = google_compute_subnetwork.default.name
+  description = "The public IP of the instance"
+  value       = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
 }
