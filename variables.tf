b456f3f3e7b16c43d8aa6710acec0adaba401067
tejas-16 <74181918+tejas-16@users.noreply.github.com>
2025-08-12 20:55:16 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,35 +1,19 @@
 variable "project_id" {
-  description = "GCP Project ID"
-  type        = string
-}
-
-variable "gcp_credentials_file" {
-  description = "Path to GCP credentials JSON"
-  type        = string
-}
-
-variable "region" {
-  description = "GCP region"
-  type        = string
-  default     = "us-central1"
-}
-
-variable "vpc_name" {
-  description = "Name of the VPC"
-  type        = string
-}
-
-variable "subnet_name" {
-  description = "Name of the subnet"
-  type        = string
-}
-
-variable "bucket_name" {
-  description = "Name of the GCS bucket"
-  type        = string
+  type = string
 }
 
 variable "instance_name" {
-  description = "Name of the Compute Engine instance"
-  type        = string
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "subnet_name" {
+  type = string
+}
+
+variable "bucket_name" {
+  type = string
 }
