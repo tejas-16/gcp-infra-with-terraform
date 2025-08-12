965b3aad1f860398291554be9dc347161812f26d
tejas-16 <74181918+tejas-16@users.noreply.github.com>
2025-08-12 20:57:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,36 +1,22 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "google" {
-  project = var.project_id
-  region  = "us-central1"
-}
-
 # Create VPC
-resource "google_compute_network" "custom_vpc" {
+resource "google_compute_network" "vpc_network" {
   name                    = var.vpc_name
   auto_create_subnetworks = false
 }
 
 # Create Subnet
-resource "google_compute_subnetwork" "custom_subnet" {
+resource "google_compute_subnetwork" "subnet" {
   name          = var.subnet_name
   ip_cidr_range = "10.0.0.0/24"
-  region        = "us-central1"
-  network       = google_compute_network.custom_vpc.id
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
 }
 
-# Create Instance
+# Create VM instance
 resource "google_compute_instance" "vm_instance" {
   name         = var.instance_name
   machine_type = "e2-micro"
-  zone         = "us-central1-a"
+  zone         = "${var.region}-a"
 
   boot_disk {
     initialize_params {
@@ -39,14 +25,14 @@ resource "google_compute_instance" "vm_instance" {
   }
 
   network_interface {
-    subnetwork = google_compute_subnetwork.custom_subnet.id
+    network    = google_compute_network.vpc_network.id
+    subnetwork = google_compute_subnetwork.subnet.id
     access_config {}
   }
 }
 
-# Create GCS Bucket
+# Create Storage Bucket
 resource "google_storage_bucket" "bucket" {
-  name          = var.bucket_name
-  location      = "US"
-  force_destroy = true
+  name     = var.bucket_name
+  location = var.region
 }
