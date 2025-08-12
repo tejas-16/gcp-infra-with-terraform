53ce623f228e3c18eb53be57e25d8a5f679cd5b3
tejas-16 <74181918+tejas-16@users.noreply.github.com>
2025-08-12 22:45:52 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,14 +5,10 @@ terraform {
       version = "~> 5.0"
     }
   }
-
-  backend "gcs" {
-    bucket = var.bucket_name_state
-    prefix = "terraform/state"
-  }
 }
 
 provider "google" {
   project = var.project_id
   region  = var.region
+  zone    = var.zone
 }
